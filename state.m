169141bcec658cc3c62f7f132fba41a8fc86eb01
Zaki Ali <zakiali88@gmail.com>
2012-03-30 00:10:11 -0700

    added new version of loopback mux. This is based on
    xilinx moore state machine block. Seems too work well.
    also added boffile for compilation and .m file that
    has the parameters put into the moore state machine block.
    detatiled explanation coming soon.

---
+++ state.m
@@ -0,0 +1,9 @@
+%this was my parameters for moore state maching in loopback fifo.
+% state 0 = Idle , 1 = Loop read, 2 = gbe read, 4 = holdoff point.
+
+nsm = [ 0 0 0 0 2 2 2 2 1 1 1 1 1 1 1 1; 1 1 3 3 1 1 3 3 1 1 3 3 1 1 3 3; 2 2 3 3 2 2 3 3 2 2 3 3 2 2 3 3; 3 0 3 0 3 0 3 0 3 0 3 0 3 0 3 0];
+
+
+outarr = [4; 2; 1; 0;];
+
+
