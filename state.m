2d924679ce1766f81d1b1deffd86692e50fc0333
Zaki Ali <zakiali88@gmail.com>
2012-04-24 14:05:38 -0700

    Added documentation files.
    Added Diagram for Current state machine.
    Added State machine table.
    state.m file updated. This is depracated.

--- state.m
+++ state.m
@@ -1,4 +1,6 @@
-%this was my parameters for moore state maching in loopback fifo.
+this was my parameters for moore state maching in loopback fifo.
+Before the design review.
+This is no longer in use. just here for reference
 % state 0 = Idle , 1 = Loop read, 2 = gbe read, 4 = holdoff point.
 
 nsm = [ 0 0 0 0 2 2 2 2 1 1 1 1 1 1 1 1; 1 1 3 3 1 1 3 3 1 1 3 3 1 1 3 3; 2 2 3 3 2 2 3 3 2 2 3 3 2 2 3 3; 3 0 3 0 3 0 3 0 3 0 3 0 3 0 3 0];
